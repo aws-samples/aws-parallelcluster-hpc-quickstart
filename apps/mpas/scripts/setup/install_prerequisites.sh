e5febd80d4e2ce2192774c8c9f4ea4a77e120e22
Maxime Hugues <maxhaws@amazon.com>
2022-03-08 16:22:34 -0600

    Changer packer version to 1.7.10
    Close #15

--- apps/mpas/scripts/setup/install_prerequisites.sh
+++ apps/mpas/scripts/setup/install_prerequisites.sh
@@ -20,8 +20,8 @@ sudo ./aws/install
 
 # Install Packer
 
-PACKER_VERSION="1.6.4"
-PACKER_VERSION_SHA256SUM="a20ec68e9eb6e1d6016481003f705babbecc28e234f8434f3a35f675cb200ea8"
+PACKER_VERSION="1.7.10"
+PACKER_VERSION_SHA256SUM="1c8c176dd30f3b9ec3b418f8cb37822261ccebdaf0b01d9b8abf60213d1205cb"
 
 echo "[INFO] Installing Packer"
 curl -O https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
